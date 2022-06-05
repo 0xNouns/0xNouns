8328d862a2f735265f5051afc4b9c3018ae37e05
0xNouns <0xnouns@proton.me>
2022-06-05 14:05:11 -0400

    index.ts

--- packages/nouns-webapp/src/pages/Nounders/index.tsx
+++ packages/nouns-webapp/src/pages/Nounders/index.tsx
@@ -87,34 +87,23 @@ const NoundersPage = () => {
   return (
     <Section fullWidth={true} className={classes.noundersPage}>
       <Col lg={{ span: 6, offset: 3 }}>
-        <h2 style={{ marginBottom: '2rem' }}>
-          <Trans>The Nounders</Trans>
-        </h2>
-        <h3 style={{ marginBottom: '2rem' }}>
-          <Trans>3.5 artists, 6.5 technologists</Trans>
-        </h3>
+        <h2 style={{ marginBottom: '2rem' }}>The 0xNounders</h2>
         <Row style={{ marginBottom: '0rem' }}>
           <BioCards min={0} max={5} />
           <BioCards min={5} max={10} />
         </Row>
-        <h3>
-          <Trans>Nounders' Reward</Trans>
-        </h3>
+        <h3>0xNounders' Reward</h3>
         <p style={{ textAlign: 'justify' }}>
-          <Trans>
             All 0xNouns auction proceeds are sent to the 0xNouns DAO. For this reason, we, the project's
             founders (‘0xNounders’) have chosen to compensate ourselves with 0xNouns. Every 10th 0xNoun
             for the first 5 years of the project will be sent to our multisig (5/10), where it will
             be vested and distributed to individual 0xNounders.
-          </Trans>
         </p>
         <p style={{ textAlign: 'justify' }}>
-          <Trans>
             The 0xNounders reward is intended as compensation for our pre and post-launch
             contributions to the project, and to help us participate meaningfully in governance as
             the project matures. Since there are 10 Nounders, after 5 years each 0xNounder could
             receive up to 1% of the 0xNoun supply.
-          </Trans>
         </p>
       </Col>
     </Section>
